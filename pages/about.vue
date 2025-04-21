44203a0d976a4554cd2589534ae650bd7162fdbf
Metis_12 <metis0987568205@gmail.com>
2025-04-21 16:14:52 +0700

    admin

--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex items-center justify-center h-screen">
-    <h1 class="text-4xl font-bold">สินค้า</h1>
+    <h1 class="text-4xl font-bold">เกี่ยวกับฉัน</h1>
   </div>
 </template>
 
